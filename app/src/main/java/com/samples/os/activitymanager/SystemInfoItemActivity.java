add460279a015b658e10017d660aa4cc04e8a4bf
java2019 <java_2019@mail.ru>
2017-07-05 17:54:35 +0300

    SystemInfoItemActivity

--- app/src/main/java/com/samples/os/activitymanager/SystemInfoItemActivity.java
+++ app/src/main/java/com/samples/os/activitymanager/SystemInfoItemActivity.java
@@ -1,7 +1,51 @@
 package com.samples.os.activitymanager;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
 /**
  * Created by use on 05.07.17.
  */
-public class SystemInfoItemActivity {
+public class SystemInfoItemActivity extends Activity {
+    private TextView text;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item);
+        text = (TextView)findViewById(R.id.text);
+        String action = this.getIntent().getAction();
+        this.setTitle(action);
+        ActivityManager manager =
+                (ActivityManager)getSystemService(ACTIVITY_SERVICE);
+        if (action.equals(
+                getResources().getString(R.string.itemDeviceConfigInfo))) {
+                // Информация о конфигурации устройства
+        }
+        else if (action.equals(
+                getResources().getString(R.string.itemMemoryInfo))) {
+                // Информация о памяти
+        }
+        else if (action.equals(
+                getResources().getString(R.string.itemRunningAppProcesses))) {
+                // Информация о выполняющихся процессах
+        }
+        else if (action.equals(
+                getResources().getString(R.string.itemRunningServices))) {
+                // Информация о выполняющихся службах
+        }
+        else if (action.equals(
+                getResources().getString(R.string.itemRunningTasks))) {
+                // Информация о выполняющихся заданиях
+        }
+        else if (action.equals(
+                getResources().getString(R.string.itemRecentTasks))) {
+                // Информация о последних выполненных заданиях
+        }
+        else if (action.equals(
+                getResources().getString(R.string.itemProcessesInErrorState))) {
+                // Информация о процессах в состоянии ошибки
+        }
+    }
 }
