3073f51fa239f2933fdf102a194f5ad6e36f1a58
java2019 <java_2019@mail.ru>
2017-07-05 17:52:39 +0300

    main activity

---
+++ app/src/main/java/com/samples/os/activitymanager/SystemInfoItemActivity.java
@@ -0,0 +1,7 @@
+package com.samples.os.activitymanager;
+
+/**
+ * Created by use on 05.07.17.
+ */
+public class SystemInfoItemActivity {
+}
--- app/src/main/java/com/samples/os/activitymanager/SystemInfoListActivity.java
+++ app/src/main/java/com/samples/os/activitymanager/SystemInfoListActivity.java
@@ -1,13 +1,40 @@
 package com.samples.os.activitymanager;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class SystemInfoListActivity extends AppCompatActivity {
+public class SystemInfoListActivity extends ListActivity {
+
+    private String[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system_info_list);
+
+        items = new String[] {
+                getResources().getString(R.string.itemDeviceConfigInfo),
+                getResources().getString(R.string.itemMemoryInfo),
+                getResources().getString(R.string.itemRunningAppProcesses),
+                getResources().getString(R.string.itemRunningServices),
+                getResources().getString(R.string.itemRunningTasks),
+                getResources().getString(R.string.itemRecentTasks),
+                getResources().getString(R.string.itemProcessesInErrorState)
+        };
+        setListAdapter(new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_list_item_1, items));
+    }
+    @Override
+    protected void onListItemClick(
+            ListView parent, View v, int position, long id) {
+        String item = items[position];
+        Intent intent = new Intent(getApplicationContext(),
+                SystemInfoItemActivity.class);
+        intent.setAction(item);
+        startActivity(intent);
     }
 }
