64822dcea88df1e1a0c76f964d76f0b9b5d84410
java2019 <java_2019@mail.ru>
2017-07-05 17:48:55 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/os/activitymanager/SystemInfoListActivity.java
@@ -0,0 +1,13 @@
+package com.samples.os.activitymanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SystemInfoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_system_info_list);
+    }
+}
